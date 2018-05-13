9a92644462d645b9c722941853ffd0aeca670972
yusuke-can <a.plague.of.locusts@gmail.com>
2018-05-14 00:20:12 +0900

    set template text to PR title and body

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,5 +2,22 @@
 
 $(function () {
  console.log('Run k-link!')
-// $('body').css('background-color', '#ff0000');
+ $('#pull_request_title').val(create_pull_requset_title_template_text())
+ $('#pull_request_body').text(create_pull_requset_body_template_text())
+
+ $('button.js-preview-tab').click()
+ $('button.js-write-tab').click()
 });
+
+/**
+ * プルリクエストタイトルテンプレートコメント生成
+ */
+function create_pull_requset_title_template_text() {
+    　return 'title template';
+}
+/**
+ * PRテンプレートコメント生成
+ */
+function create_pull_requset_body_template_text() {
+　return 'body template';
+}
